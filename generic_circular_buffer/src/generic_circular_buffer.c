133dd6db7d6a19c87eefeae0af59dd6f41115114
Ryan Bartling <ryan.bartling@gmail.com>
2016-12-09 17:32:59 -0700

    Created project for a generic circular buffer in c
    Started outlining tests to write for this project

---
+++ generic_circular_buffer/src/generic_circular_buffer.c
@@ -0,0 +1,37 @@
+
+//
+// Section: Included Files
+//
+
+#include "generic_circular_buffer.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+//
+// Section: Constants
+//
+
+//
+// Section: Global Variable Declarations
+//
+
+//
+// Section: Macros
+//
+
+//
+// Section: Static Function Prototypes
+//
+
+//
+// Section: Static Function Definitions
+//
+
+//
+// Section:  Module APIs
+//
+
+//
+// End of File
+//
---
+++ generic_circular_buffer/src/generic_circular_buffer.h
@@ -0,0 +1,28 @@
+
+#ifndef GENERIC_CIRCULAR_BUFFER_H    // Guards against multiple inclusion
+#    define GENERIC_CIRCULAR_BUFFER_H
+
+//
+// Section: Included Files
+//
+
+#    include <stdbool.h>
+#    include <stdint.h>
+
+//
+// Section: Constants
+//
+
+//
+// Section: Data Types
+//
+
+//
+// Section:  Module APIs
+//
+
+#endif
+
+//
+// End of File
+//
